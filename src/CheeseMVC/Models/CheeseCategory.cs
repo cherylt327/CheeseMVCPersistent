f9d187b8bf1cb9ac13fe5369a7084191a839f829
cherylt327 <cherylt327@yahoo.com>
2018-10-07 16:20:14 -0500

    Step2

--- src/CheeseMVC/Models/CheeseCategory.cs
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+
 namespace CheeseMVC.Models
 {
     public class CheeseCategory
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public IList<Cheese> Cheeses { get; set; }
     }
 }
